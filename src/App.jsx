59f15baf3c3c7dc2ff22d0a1ffc8740332fb1565
saima67 <meafreen09@gmail.com>
2026-03-04 15:26:59 +0600

    added footer  section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import NavBar from "./Components/Navbar/NavBar";
+import Footer from "./Components/Footer/footer";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <NavBar></NavBar>
+      <Footer></Footer>
     </>
   );
 }
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="bg-[#632ee3] md:px-53 py-7">
+        <div className="flex flex-col md:flex-row justify-between px-10 md:px-0 py-5">
+          <div className="md:basis-sm">
+            <h2 className="text-white font-bold text-lg mb-3">
+              TS_TicketXpress
+            </h2>
+            <p className="text-white text-sm">
+              Our ticketing system is a tool used to track and manage customer
+              issues or service requests. It creates a unique ticket for each
+              problem, helping teams organize, prioritize, and resolve tasks
+              efficiently.
+            </p>
+          </div>
+          <div className="text-white md:basis-sm md:pl-20 mt-5 md:mt-0">
+            <h2 className="font-semibold">Company</h2>
+            <ul className="text-white font-semibold">
+              <li className="py-1 font-normal">Abut Us</li>
+              <li className="py-1 font-normal">Our Mission</li>
+              <li className="py-1 font-normal">Contact Us</li>
+            </ul>
+          </div>
+          <div className="md:basis-sm mt-5 md:mt-0">
+            <h2 className="text-white font-semibold">Services</h2>
+            <ul className="text-white font-semibold">
+              <li className="py-1 font-normal">Products & Services</li>
+              <li className="py-1 font-normal">Customer Stories</li>
+              <li className="py-1 font-normal">Download Apps</li>
+            </ul>
+          </div>
+          <div className="md:basis-sm mt-5 md:mt-0">
+            <h2 className="text-white font-semibold">Information</h2>
+            <ul className="text-white font-semibold">
+              <li className="py-1 font-normal">Privacy Policy</li>
+              <li className="py-1 font-normal">Terms & Conditions</li>
+              <li className="py-1 font-normal">Join Us</li>
+            </ul>
+          </div>
+          <div className="md:basis-sm mt-5 md:mt-0">
+            <h2 className="text-white font-semibold">Social Links</h2>
+            <ul className="text-white font-semibold">
+              <li className="py-1 font-normal">Ticket System</li>
+              <li className="py-1 font-normal">Ticket System</li>
+              <li className="py-1 font-normal">Ticket System</li>
+              <li className="py-1 font-normal">support@tst.com</li>
+            </ul>
+          </div>
+        </div>
+        <div className="border border-white"></div>
+        <div className="flex flex-col-reverse md:flex-row justify-center px-5 py-5">
+          <p className="text-white">
+            &copy; 2026 VintageShop,Inc. All Rights Reserved.
+          </p>
+          <ul className="flex space-x-2">
+            <li>
+              <i className="fa-brands fa-facebook-f text-gray-400"></i>
+            </li>
+            <li>
+              <i className="fa-brands fa-twitter text-gray-400"></i>
+            </li>
+            <li>
+              <i className="fa-brands fa-instagram text-gray-400"></i>
+            </li>
+            <li>
+              <i className="fa-brands fa-linkedin text-gray-400"></i>
+            </li>
+            <li>
+              <i className="fa-regular fa-envelope text-gray-400"></i>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
